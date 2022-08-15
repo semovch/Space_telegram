5d36d1c657f04b9b7e5cce18756d934135ec9242
semovch <107846695+semovch@users.noreply.github.com>
2022-08-15 18:27:56 +0600

    Поправил порядок слов в переменных
    вместо number_apod и response_apod поставил apod_number и apod_response

--- nasa_apod.py
+++ nasa_apod.py
@@ -16,14 +16,14 @@ def save_nasa_apod(dir_path, api_key):
     response = requests.get(url, params=parameters)
     response.raise_for_status
     apods = response.json()
-    for number_apod, apod in enumerate(apods):
+    for apod_number, apod in enumerate(apods):
         apod_ext = split(apod["url"])
         os.makedirs(dir_path, exist_ok=True)
-        filename = f'nasa{number_apod}{apod_ext}'
-        response_apod = requests.get(apod['url'])
-        response_apod.raise_for_status()
+        filename = f'nasa{apod_number}{apod_ext}'
+        apod_response = requests.get(apod['url'])
+        apod_response.raise_for_status()
         with open(os.path.join(dir_path, filename), 'wb') as file:
-            file.write(response_apod.content)
+            file.write(apod_response.content)
 
 
 def main():
