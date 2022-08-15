dd123ffe4db575e4468d8f8851afb3b746f0051c
semovch <107846695+semovch@users.noreply.github.com>
2022-08-15 19:09:06 +0600

    Избавился от лишнего кода
    Убрал доп. функцию split

--- nasa_apod.py
+++ nasa_apod.py
@@ -3,7 +3,6 @@ import os
 
 import requests
 
-from my_module import split
 from dotenv import load_dotenv
 
 
@@ -17,7 +16,7 @@ def save_nasa_apod(dir_path, api_key):
     response.raise_for_status
     apods = response.json()
     for apod_number, apod in enumerate(apods):
-        apod_ext = split(apod["url"])
+        apod_ext = os.path.splitext(apod["url"])[1]
         os.makedirs(dir_path, exist_ok=True)
         filename = f'nasa{apod_number}{apod_ext}'
         apod_response = requests.get(apod['url'])
