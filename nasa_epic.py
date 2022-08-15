ae36e81dcc5621fbd2bad8e6a6a1bd4e327d5b42
semovch <107846695+semovch@users.noreply.github.com>
2022-08-15 18:05:07 +0600

    Изменил название переменной
    Изменил название пременной dir_name а dir_path, т.к. это путь а не имя

--- nasa_epic.py
+++ nasa_epic.py
@@ -8,19 +8,18 @@ import json
 from dotenv import load_dotenv
 
 
-def save_nasa_epic(dir_name, api_key):
+def save_nasa_epic(dir_path, api_key):
     url = f"https://api.nasa.gov/EPIC/api/natural/images?api_key={api_key}"
     response = requests.get(url)
     response_json = json.loads(response.text)
     for number_epic, epic in enumerate(response_json[:5]):
-        dir_name = 'epic'
-        os.makedirs(dir_name, exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
         filename = f'epic{number_epic}.png'
         date_epic = datetime.date.fromisoformat(epic['date'][:10]).strftime("%Y/%m/%d")
         image_epic = epic["image"]
         response_epic = requests.get(f"https://api.nasa.gov/EPIC/archive/natural/{date_epic}/png/{image_epic}.png?api_key={api_key}")
         response_epic.raise_for_status()
-        with open(os.path.join(dir_name, filename), 'wb') as file:
+        with open(os.path.join(dir_path, filename), 'wb') as file:
             file.write(response_epic.content)
             
 
@@ -29,11 +28,11 @@ def main():
     parser = argparse.ArgumentParser(
         description='скачивание картинок EPIC от NASA'
     )
-    parser.add_argument('dir_name', help='введите путь к директории')
+    parser.add_argument('dir_path', help='введите путь к директории')
     args = parser.parse_args()
-    dir_name = args.dir_name
+    dir_path = args.dir_path
     api_key = os.environ['API_KEY_NASA']
-    return(save_nasa_epic(dir_name))
+    return(save_nasa_epic(dir_path, api_key))
 
 
 if __name__ == '__main__':
