477875fc6c5fa3c0d6965c77bd8ad1a3286f05fd
semovch <107846695+semovch@users.noreply.github.com>
2022-08-18 18:37:16 +0600

    Изменил название переменной
    Использовал правильный регистр для переменной отвечающей за id запуска SpaceX

--- spacex_launch.py
+++ spacex_launch.py
@@ -4,17 +4,17 @@ import os
 import requests
 
 
-def fetch_spacex_last_launch(ID, dir_name):
-    response_launch = requests.get(f"https://api.spacexdata.com/v5/launches/{ID}")
+def fetch_spacex_last_launch(dir_path, launch_id):
+    response_launch = requests.get(f"https://api.spacexdata.com/v5/launches/{launch_id}")
     response_launch.raise_for_status()
-    images = json.loads(response_launch.text)['links']['flickr']['original']
+    images = response_launch.json()['links']['flickr']['original']
     for image_numb, image in enumerate(images):
         image_ext = os.path.splitext(image)[1]
-        os.makedirs(dir_name, exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
         filename = f'spacex{image_numb}{image_ext}'
         response_image = requests.get(image)
         response_image.raise_for_status()
-        with open(os.path.join(dir_name, filename), 'wb') as file:
+        with open(os.path.join(dir_path, filename), 'wb') as file:
             file.write(response_image.content)
 
 
@@ -22,12 +22,12 @@ def main():
     parser = argparse.ArgumentParser(
         description='скачивание картинок'
     )
-    parser.add_argument('dir_name', help='введите путь к директории')
-    parser.add_argument('--ID', help='введите ID запуска', default="latest")
+    parser.add_argument('dir_path', help='введите путь к директории')
+    parser.add_argument('--launch_id', help='введите ID запуска', default="latest")
     args = parser.parse_args()
-    dir_name = args.dir_name
-    ID = args.ID
-    return(fetch_spacex_last_launch(ID, dir_name))
+    dir_path = args.dir_path
+    launch_id = args.launch_id
+    return(fetch_spacex_last_launch(dir_path, launch_id))
 
 
 if __name__ == '__main__':
